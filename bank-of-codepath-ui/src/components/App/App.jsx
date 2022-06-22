2205dfdfb9041b90d112d8131af8aff983f243ee
Annesa Tran <annesat2020@gmail.com>
2022-06-22 09:34:55 -0700

    Setup routing and state and handler variables

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,31 @@
 import * as React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
+import TransactionalDetail from "../TransactionDetail/TransactionDetail"
 import "./App.css"
 
 export default function App() {
+
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [transactions, setTransactions] = React.useState([])
+  const [transfers, setTransfers] = React.useState([])
+  const [error, setError] = React.useState(null)
+  const [filterInputValue, setFilterInputValue] = React.useState("")
+
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={
+              <Home /> } />
+            <Route path="/transactions/:transactionId" element={
+              <TransactionalDetail /> } />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
